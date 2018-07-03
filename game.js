635dfaf4f2833947eaf08b2b023ce4c1b30e60c6
Katarzyna Berbeka <katarzyna.berbeka@gmail.com>
2018-07-03 16:22:52 +0200

    js - finished

--- game.js
+++ game.js
@@ -33,20 +33,54 @@ let gameResult = 0;
 const clickCard = function() {
   /*one full round*/
   activeCard = this;
+
+  //block double click on one card
+  if (activeCard == activeCards[0]) return;
+
   activeCard.classList.remove("hidden");
   //check if first click
   if (activeCards.length === 0) {
-    activeCards[0] == activeCard;
+    activeCards[0] = activeCard; //dziala bo activecard dostal thisa
     return;
   }
   //if second click - block clicking on other cards
   else {
-  }
+    cards.forEach(card => {
+      card.removeEventListener("click", clickCard);
+      activeCards[1] = activeCard;
+      /*delay to show both cards */
+      setTimeout(function() {
+        //if second click - check if win or loose. if fin check if game over
+        if (activeCards[0].className === activeCards[1].className) {
+          console.log("round won");
+          activeCards.forEach(card => card.classList.add("off"));
+          //result increment
+          gameResult++;
 
-  //if second click - block for click
-  //if second click - check if win or loose. if fin check if game over
-  //block double click on one card
-  //block click on class hidden
+          //exclude clicked cards from cards array
+          cards = cards.filter(card => !card.classList.contains("off"));
+
+          //check if game over
+          if (gameResult == gamePairs) {
+            const endTime = new Date().getTime();
+            const gameTime = (endTime - startTime) / 1000;
+            alert(`You won! Your score: ${gameTime} seconds`);
+            location.reload();
+          }
+        } else {
+          console.log("round lost");
+          activeCards.forEach(card => card.classList.add("hidden"));
+        }
+
+        //active cards reset
+        activeCard = "";
+        activeCards.length = 0;
+
+        //bring back event listener
+        cards.forEach(card => card.addEventListener("click", clickCard));
+      }, 500);
+    });
+  }
 };
 
 const init = function() {
